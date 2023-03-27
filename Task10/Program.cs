0c300855fc0dd712ba4470b685cb0830331c94b6
SvyatGysto <svyatgysto@gmail.com>
2023-03-28 00:20:09 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿int CutNumber(int num)
+{
+    int hunders = num % 100;
+    int units = num % 10;
+    int result = (hunders - units)/10;
+    return result;
+}
+Console.WriteLine("Введите трехзначное число");
+int numOne= Convert.ToInt32(Console.ReadLine());
+int newNumber=CutNumber(numOne);
+Console.WriteLine("Вторая цифра числа");
+Console.WriteLine(newNumber);
