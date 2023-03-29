b604ddd19ee20c1ae79e26b9fab461abb50db231
SvyatGysto <svyatgysto@gmail.com>
2023-03-29 14:25:53 +0300

    task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,29 @@
+﻿bool DayOff(int num)
+{
+    if(num <= 5)
+    {
+        return false;
+    }
+    else
+    {
+            return true;
+    }
+}
+Console.WriteLine("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>7)
+{
+    Console.WriteLine("Введите число от 1 до 7!!!");
+}
+else
+{
+    bool examination = DayOff(number);
+    if (examination == false)
+    {
+        Console.WriteLine("Данный день недели - рабочий");
+    }
+    else
+    {
+        Console.WriteLine("Данный день недели - выходной");
+    }
+}    
