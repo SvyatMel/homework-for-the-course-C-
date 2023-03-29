d96bea6d44aad1d50405d862270a5606e2bce104
SvyatGysto <svyatgysto@gmail.com>
2023-03-29 13:45:39 +0300

    task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}*/ 
+// Без Метода
+int OneNum (int numThree)
+{
+    if(numThree<100)
+    {
+        return numThree;
+    }
+    else
+    {
+        while(numThree>1000)
+        {
+            numThree=numThree /10;
+        }
+        return numThree %10 ;
+    }
+}
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num3=OneNum(number);
+if(number==OneNum(number))
+{
+     Console.WriteLine("-> третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра заданного числа "+ num3);
+}
