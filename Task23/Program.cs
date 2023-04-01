f8f05b66146d428507482e1ff8b5c7758419ea59
SvyatGysto <svyatgysto@gmail.com>
2023-04-02 01:13:03 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿void Cubed(int num)
+{
+    for(int i = 1 ;i <= num;i++)
+    {
+        int y=3;
+        Console.WriteLine(Math.Pow(i,y));
+    }
+}
+Console.Write("Введите число: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Cubed(num1);
