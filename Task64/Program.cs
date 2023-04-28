a8e87d20f81c4cf9022b635a22eb4d8568428ca1
SvyatoslavMelkov <svyatgysto@gmail.com>
2023-04-28 21:02:05 +0300

    Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте значение N.Напишите программу , которая выведет все натуральные числа в диапозоне от N до 1
+void CreadNum(int num)
+{
+    Console.Write(num + " ");
+    if(num>1) CreadNum(num-1);  
+}
+Console.Write("Введите число N: ");
+int num=Convert.ToInt32(Console.ReadLine());
+CreadNum(num);
